c8de7379b03ed22fe858526ed45b075ed928b223
satoshi-komatu <k023g1069@g.neec.ac.jp>
2024-01-31 10:45:35 +0900

    pullを入れた

--- Program.vb
+++ Program.vb
@@ -3,5 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello git")
+        Console.WriteLine("PullRequest branch changed")
+
     End Sub
 End Module
